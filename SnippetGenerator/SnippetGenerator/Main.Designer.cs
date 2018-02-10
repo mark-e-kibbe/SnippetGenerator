ef05e7e2be0f2a24eaf540ed17ccee5080dd5892
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-10 16:10:36 -0500

    Closes #2 : Created metadata panel and children

--- SnippetGenerator/SnippetGenerator/Main.Designer.cs
+++ SnippetGenerator/SnippetGenerator/Main.Designer.cs
@@ -139,7 +139,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 408);
+            this.ClientSize = new System.Drawing.Size(478, 395);
             this.Controls.Add(this.pnlMetaData);
             this.Name = "Main";
             this.Text = "Snippet Generator";
