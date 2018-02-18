8bbc00012bc12650f73b26c887cd802647c1bf40
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-18 14:33:26 -0500

    Created Metadata and Literal Models

---
+++ SnippetGenerator/SnippetGenerator/Models/Literal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnippetGenerator.Models
+{
+    public class Literal
+    {
+        public Literal(string ID, string ToolTip, string Default)
+        {
+            _ID = ID;
+            _ToolTip = ToolTip;
+            _Default = Default;
+        }
+
+        public string _ID { get; set; }
+        public string _ToolTip { get; set; }
+        public string _Default { get; set; }
+    }
+}
---
+++ SnippetGenerator/SnippetGenerator/Models/Metadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnippetGenerator.Models
+{
+    public class Metadata
+    {
+        public Metadata(string Title, string Shortcut, string Description, string Author)
+        {
+            _Title = Title;
+            _Shortcut = Shortcut;
+            _Description = Description;
+            _Author = Author;
+        }
+
+        public string _Title {get;set;}
+        public string _Shortcut {get;set;}
+        public string _Description {get;set;}
+        public string _Author {get;set;}
+    }
+}
