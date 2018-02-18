bf6d100b94b19e4f5f4219e471b91048573abf3c
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-18 13:21:22 -0500

    Created static utilities class files for helpers, method from issue 10 in

---
+++ SnippetGenerator/SnippetGenerator/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnippetGenerator
+{
+    public static class Utilities
+    {
+        public static string ValueOfChecked(Panel container)
+        {
+            return ValueOfChecked(container);
+        }
+        public static string ValueOfChecked(GroupBox container)
+        {
+            return ValueOfChecked(container);
+        }
+        private static string ValueOfChecked(Control container)
+        {
+            foreach (Control control in container.Controls)
+            {
+                RadioButton rb = (RadioButton)control;
+                if (rb.Checked == true)
+                {
+                    return rb.Text;
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            //TODO Hardcoded strings I feel should not be used, change if possible
+            return "None";
+        }
+    }
+}
