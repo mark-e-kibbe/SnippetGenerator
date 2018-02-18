8091311a01877b9c3836a583353f3b94ed67c943
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-18 11:47:58 -0500

    Added Initialization Logic, Enums for rad button default properties

--- SnippetGenerator/SnippetGenerator/Models/Configuration.cs
+++ SnippetGenerator/SnippetGenerator/Models/Configuration.cs
@@ -9,26 +9,43 @@ namespace SnippetGenerator.Models
 {
     public class Configuration
     {
+        public enum PlatformEnums
+        {
+            VS,
+            SSMS
+        }
+        public  enum SnippetTypeEnums
+        {
+            SurroundsWith,
+            Expansion
+        }
+
         public string VStudioOutputFilePath { get; set; }
         public string SSMSOutputFilePath { get; set; }
         public string Author { get; set; }
         public bool IsFirstTimeRun { get; set; }
-
+        public PlatformEnums Platform { get; set; }
+        public  SnippetTypeEnums SnippetType { get; set; }
         public Configuration()
         {
             VStudioOutputFilePath = Default.VisualStudioSnippetFilePath;
             SSMSOutputFilePath = Default.SSMSSnippetFilePath;
             Author = Default.Author;
             IsFirstTimeRun = Default.IsFirstRun;
+            Platform = (PlatformEnums)Enum.Parse(typeof(PlatformEnums), Default.Platform);
+            SnippetType = (SnippetTypeEnums)Enum.Parse(typeof(SnippetTypeEnums), Default.SnippetType);
         }
 
         public void UpdateConfiguration()
         {
-            Default.VisualStudioSnippetFilePath =VStudioOutputFilePath;
+            Default.VisualStudioSnippetFilePath = VStudioOutputFilePath;
             Default.SSMSSnippetFilePath = SSMSOutputFilePath;
             Default.Author = Author;
             Default.IsFirstRun = false;
+            Default.Platform = Platform.ToString();
+            Default.SnippetType = SnippetType.ToString();
             Default.Save();
         }
     }
+    
 }
