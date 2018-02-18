9b5c75d56c1992329e1c091fae03be51aa6e7236
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-18 14:44:32 -0500

    Created Enumerations folder, must move enumerations out of configuration class

---
+++ SnippetGenerator/SnippetGenerator/Snippet Enumerations/SnippetEnums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnippetGenerator.Snippet_Enumerations
+{
+    public static class SnippetEnums
+    {
+
+    }
+}
