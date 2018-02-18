22e22ef684550870b89f47b6531ca0c5fcdae01f
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-18 14:58:18 -0500

    Added properties for Snippet Model Class

--- SnippetGenerator/SnippetGenerator/Models/Snippet.cs
+++ SnippetGenerator/SnippetGenerator/Models/Snippet.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SnippetGenerator.Snippet_Enumerations.SnippetEnums;
 
 namespace SnippetGenerator.Models
 {
     public class Snippet
     {
-        Metadata _Metadata = new Metadata();
-        List<Literal> _Literals = new List<Literal>();
+        public PlatformEnums _Platform { get; set; }
+        public SnippetTypeEnums _SnippetType { get; set; }
+
+        public Metadata _Metadata = new Metadata();
+        public List<Literal> _Literals = new List<Literal>();
+
+        //TODO Class this property out
+        public string CodeToSnippet = string.Empty;
     }
 }
