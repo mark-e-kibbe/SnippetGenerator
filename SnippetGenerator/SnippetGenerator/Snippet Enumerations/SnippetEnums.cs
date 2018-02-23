582c164f3c3c15ce76e564f3c16cf021144ad4e7
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-22 19:44:55 -0500

    XML Commented Enumerations file, also explained SnippetType

--- SnippetGenerator/SnippetGenerator/Snippet Enumerations/SnippetEnums.cs
+++ SnippetGenerator/SnippetGenerator/Snippet Enumerations/SnippetEnums.cs
@@ -6,14 +6,28 @@ using System.Threading.Tasks;
 
 namespace SnippetGenerator.Snippet_Enumerations
 {
+    /// <summary>
+    /// Noninstanced reference class for properties that should rarely/never change
+    /// </summary>
     public static class SnippetEnums
     {
+        /// <summary>
+        /// List of possible Target Platforms for the Code Snippet
+        /// </summary>
         public enum PlatformEnums
         {
             None,
             SSMS,
             VS
         }
+
+        /// <summary>
+        /// List of The type of snippet desired
+        /// </summary>
+        /// <remarks>
+        /// Expansion is used for dropping in preset code
+        /// SurroundsWith is used for surrounding selected code with a code snippet such as try catch, if blocks, etc
+        /// </remarks>
         public enum SnippetTypeEnums
         {
             Expansion,
