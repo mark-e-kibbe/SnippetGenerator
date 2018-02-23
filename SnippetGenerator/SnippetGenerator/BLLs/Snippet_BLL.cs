da73af1f6de6d818fadd4629a4d1b73abc627862
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-22 19:18:30 -0500

    Completed snippet models and BLL XML Comments.

--- SnippetGenerator/SnippetGenerator/BLLs/Snippet_BLL.cs
+++ SnippetGenerator/SnippetGenerator/BLLs/Snippet_BLL.cs
@@ -10,10 +10,19 @@ using static SnippetGenerator.Snippet_Enumerations.SnippetEnums;
 
 namespace SnippetGenerator.BLLs
 {
+    /// <summary>
+    /// Class for use of logic by Snippet Model
+    /// </summary>
     public class Snippet_BLL
     {
+        /// <summary>
+        /// Creates validated and formatted Code Snippet XDocument object
+        /// </summary>
+        /// <param name="snippet">The snippet model to convert into a XML Code Snippet</param>
+        /// <returns>XDocument containing Validated XML Code Snippet</returns>
         private XDocument GenerateSnippet(Snippet snippet)
         {
+            //Used to convert the xml string into validated and pretty printed XML
             XDocument CompletedXML = new XDocument();
 
             //start basic no literals
@@ -26,7 +35,7 @@ namespace SnippetGenerator.BLLs
             _stringBuilder.AppendLine(@"<Snippet>");
             _stringBuilder.AppendLine(@"<Declarations>");
 
-            //for each loop to get do each literal in the list
+            //for each loop to create each literals XML in the list of Literals
             if (snippet._Literals != null && snippet._Literals.Count > 0)
             {
                 foreach (Literal literal in snippet._Literals)
@@ -37,23 +46,34 @@ namespace SnippetGenerator.BLLs
 
             _stringBuilder.AppendLine(@"</Declarations>");
 
+            //Appends the code the user would like to turn into a code snippet to the xml string being built
             _stringBuilder.AppendLine(CodeXML(snippet.CodeToSnippet, snippet._Platform));
+
             _stringBuilder.AppendLine(@"</Snippet>");
             _stringBuilder.AppendLine(@"</CodeSnippet>");
             _stringBuilder.AppendLine(@"</CodeSnippets>");
 
+            //Parse and set the completed XML Code Snippet string to XDocument
             CompletedXML = ParseXMLStr(_stringBuilder.ToString());
-
 
             return CompletedXML;
         }
 
+        /// <summary>
+        /// Saves the code snippet to path provided - path includes filename
+        /// </summary>
+        /// <param name="snippetToSave">The Snippet to turn into an XML Code Snippet file</param>
+        /// <param name="path">Path and filename the snippet will be stored at</param>
         public void Save(Snippet snippetToSave, string path)
         {
             XDocument xdocSnippet = GenerateSnippet(snippetToSave);
             xdocSnippet.Save(path);
         }
 
+        /// <summary>
+        /// Generates XML string of standard metadata xml definitions - not the Metadata itself
+        /// </summary>
+        /// <returns>XML string for Metadata definitions</returns>
         private string LocDefinitionXML()
         {
             string LocDef = string.Empty;
@@ -73,6 +93,11 @@ namespace SnippetGenerator.BLLs
             return LocDef;
         }
 
+        /// <summary>
+        /// Generates XML string of metadata xml
+        /// </summary>
+        /// <param name="metadata">The metadata to convert to xml string</param>
+        /// <returns>XML string of Metadata</returns>
         private string HeaderXML(Metadata metadata)
         {
             string HeaderXML = string.Empty;
@@ -84,6 +109,11 @@ namespace SnippetGenerator.BLLs
             return HeaderXML;
         }
 
+        /// <summary>
+        /// Generates XML String of a Literal Model
+        /// </summary>
+        /// <param name="literal">The literal to convert to a XML string</param>
+        /// <returns>XML string of Literal</returns>
         private string LiteralXML(Literal literal)
         {
             string LiteralXML = string.Empty;
@@ -95,9 +125,16 @@ namespace SnippetGenerator.BLLs
             return LiteralXML;
         }
 
+        /// <summary>
+        /// Converts the code to turn into a Code Snippet to XML string
+        /// </summary>
+        /// <param name="code">Code to convert to snippet</param>
+        /// <param name="platform">Target platform for Code Snippet - SSMS or VS</param>
+        /// <returns>Generated XML string for code to snippet</returns>
         private string CodeXML(string code, Snippet_Enumerations.SnippetEnums.PlatformEnums platform)
         {
             string CodeXML = string.Empty;
+
             //determine which enum, set string to the appropriate required attribute of Language Property
             string CodeLanguageAttribute = platform == PlatformEnums.SSMS ? "SQL" : "csharp";
 
@@ -115,6 +152,11 @@ namespace SnippetGenerator.BLLs
             return CodeXML;
         }
 
+        /// <summary>
+        /// Converts a complete xml code snippet string to a validated XML XDocument
+        /// </summary>
+        /// <param name="xmlStr">the xml to parse into a validated XML Xdocument object</param>
+        /// <returns></returns>
         private XDocument ParseXMLStr(string xmlStr)
         {
             try
--- SnippetGenerator/SnippetGenerator/Models/Snippet.cs
+++ SnippetGenerator/SnippetGenerator/Models/Snippet.cs
@@ -9,16 +9,29 @@ using static SnippetGenerator.Snippet_Enumerations.SnippetEnums;
 
 namespace SnippetGenerator.Models
 {
+    /// <summary>
+    /// Complete SSMS and VS Snippet Model
+    /// </summary>
     public class Snippet
     {
         private Snippet_BLL snippet_BLL = new Snippet_BLL();
 
-        public string CodeToSnippet { get; set;}
+        /*
+         * Platform: Targeted Platform for Code Snippet - SSMS or VS
+         * Metadata: Model containing everything required to make Metadata portion of snippet
+         * Literals: List containing literal models to be used in the Code Snippet
+         * CodeToSnippet: The code to be turned into a code snippet
+        */
+        public string CodeToSnippet { get; set; }
 
         public PlatformEnums _Platform { get; set; }
         public Metadata _Metadata = new Metadata();
         public List<Literal> _Literals = new List<Literal>();
 
+        /// <summary>
+        /// Saves the Code Snippet to the location passed in with the filename being Metadata Title
+        /// </summary>
+        /// <param name="path">The location the snippet will be saved along with filename</param>
         public void Save(string path)
         {
             snippet_BLL.Save(this, path);
