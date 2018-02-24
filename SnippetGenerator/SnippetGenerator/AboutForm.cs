0f2fe802fa2d32d8bab7d7de507f09f2a94e1c1e
mark-e-kibbe <mark.e.kibbe@gmail.com>
2018-02-24 11:26:51 -0500

    Commenting and rearrangement complete, going to test Toast Style notification

--- SnippetGenerator/SnippetGenerator/AboutForm.cs
+++ SnippetGenerator/SnippetGenerator/AboutForm.cs
@@ -10,34 +10,60 @@ using System.Windows.Forms;
 
 namespace SnippetGenerator
 {
+    /// <summary>
+    /// About Form for showing project information
+    /// </summary>
     public partial class AboutForm : Form
     {
         private const string GitHubLink = @"https://github.com/mark-e-kibbe/SnippetGenerator";
         private const string LinkedInLink = @"https://www.linkedin.com/in/mark-kibbe-729629122/";
 
+        /// <summary>
+        /// Basic constructor for generating form components
+        /// </summary>
         public AboutForm()
         {
             InitializeComponent();
         }
 
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void linkLblGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            System.Diagnostics.Process.Start(GitHubLink); 
-        }
-
+        /// <summary>
+        /// Copies email to cliboard and displays a notification
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void linkLblEmailAddress_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Clipboard.SetText(linkLblEmailAddress.Text);
         }
 
+        /// <summary>
+        /// Opens Browser and sends user to GitHub Repo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLblGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(GitHubLink); 
+        }
+
+        /// <summary>
+        /// Opens browser and sends user to Linked In Profile
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void linkLblLinkedIn_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start(LinkedInLink);
+        }
+
+        /// <summary>
+        /// Closes the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
